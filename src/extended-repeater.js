d41e6f4411d75dcb4b73339d2f4bdf1f5f485815
ALLaTk <regicter1810@mail.ru>
2022-04-04 15:25:00 +0300

    complete task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+
+  if (typeof options['repeatTimes'] === "undefined") options.repeatTimes = 1;
+  if (typeof options['separator'] === "undefined")  options.separator = '+';
+  if (typeof options['addition'] === "undefined") options.addition = '';
+  if (typeof options['additionRepeatTimes'] === "undefined") options.additionRepeatTimes = 1;
+  if (typeof options['additionSeparator'] === "undefined") options.additionSeparator = '|';
+
+  let s = (str+(options.addition+options.additionSeparator).repeat(options.additionRepeatTimes-1)+options.addition+options.separator).repeat(options.repeatTimes-1)+str+(options.addition+options.additionSeparator).repeat(options.additionRepeatTimes-1)+options.addition;
+
+  return s;
+
 }
 
 module.exports = {
