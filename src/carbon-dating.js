a2de896a90a3fccf9c450a71a57ab3ae88660e2d
ALLaTk <regicter1810@mail.ru>
2022-04-26 23:36:29 +0300

    complete task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,17 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+
+  const MODERN_ACTIVITY = 15;
+  const HALF_LIFE_PERIOD = 5730;
+  const k = 0.693;
+
+  if (typeof sampleActivity !== 'string' || +sampleActivity === 0 || isNaN(+sampleActivity)
+      || sampleActivity < 0 || sampleActivity > 15) {
+        return false;
+  }
+  else return Math.ceil((Math.log(MODERN_ACTIVITY/+sampleActivity)) / (k/HALF_LIFE_PERIOD));
 }
 
 module.exports = {
