f385541ba3de17edaa1273b31b5e3f95dd0b87d9
ALLaTk <regicter1810@mail.ru>
2022-04-28 16:44:09 +0300

    complete task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,113 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+   constructor(reverse = true) {
+    this.reverse = reverse;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+
+    if (message === undefined || key === undefined){
+      throw new Error('Incorrect arguments!');
+    }
+  
+    const messageUpper = message.toLocaleUpperCase();
+    const keyUpper = key.toLocaleUpperCase();
+    const alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const str = keyUpper.repeat(Math.ceil(messageUpper.length / keyUpper.length));
+  
+    const index = [];
+    const result = [];
+    const code = [];
+  
+    for (let i = 0; i < messageUpper.length; i++) {
+      if (alph.indexOf(messageUpper[i]) === -1){
+       index.push(messageUpper[i]);
+      } else {
+       index.push(alph.indexOf(messageUpper[i]));
+      };
+    };
+   
+    let  count = 0;
+  
+    for (let i = 0; i < index.length; i++) {
+     if (typeof index[i] !== 'number'){ 
+       result.push(index[i]) ;
+       count--;
+     } else {
+       result.push(index[i] + alph.indexOf(str[count]));
+     };
+      count++;
+    };
+    
+    result.forEach(el => {
+      if (typeof el !== 'number'){
+        code.push(el);
+      } else if (el >= alph.length){
+        code.push(alph[el - alph.length]);
+      } else {
+        code.push(alph[el]);
+      };
+    });
+     
+    if(this.reverse){
+      return code.join('');
+    } else{
+      return code.reverse().join('');
+    }
+  }
+
+  decrypt(message, key) {
+
+    if (message === undefined || key === undefined){
+      throw new Error('Incorrect arguments!');
+    }
+
+    const messageUpper = message.toLocaleUpperCase();
+    const keyUpper = key.toLocaleUpperCase();
+    const alph = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const str = keyUpper.repeat(Math.ceil(messageUpper.length / keyUpper.length));
+    
+    const index = [];
+    const result = [];
+    const code = [];
+  
+    for (let i = 0; i < messageUpper.length; i++) {
+      if (alph.indexOf(messageUpper[i]) === -1){
+       index.push(messageUpper[i])
+      } else {
+       index.push(alph.indexOf(messageUpper[i]))
+      }
+    } 
+   
+    let  count = 0;
+  
+    for (let i = 0; i < index.length; i++) {
+     if (typeof index[i] !== 'number'){ 
+       result.push(index[i]) 
+       count--
+     } else {
+       result.push(index[i] - alph.indexOf(str[count]))
+     }
+      count++
+    }
+  
+    result.forEach(el => {
+      if (typeof el !== 'number'){
+        code.push(el)
+      } else if (el < 0){
+        code.push(alph[alph.length + el])
+      } else {
+        code.push(alph[el])
+      }
+    });
+ 
+    if(this.reverse){
+      return code.join('');
+    } else{
+      return code.reverse().join('');
+    }
   }
 }
 
